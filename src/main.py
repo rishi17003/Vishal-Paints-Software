63bed48c63e7ac4182a9593549138720976fd880
rishi17003 <102285269+rishi17003@users.noreply.github.com>
2024-12-08 12:15:31 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,12 +1,25 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+from HomeScreen import HomeScreen
+from reportlab.pdfgen import canvas
+from PyQt5.QtGui import QPixmap, QFont,QIcon
 from product_rate_calculator import ProductRateCalculatorApp
+from ProductHistoryScreen import ProductHistoryScreen
 
 def main():
     app = QApplication(sys.argv)
-    window = ProductRateCalculatorApp()
-    window.show()
-    sys.exit(app.exec_())
+    app.setWindowIcon(QIcon("vishal_icon.ico"))
+    home_screen = HomeScreen()
+
+    # Check if login was successful
+    if home_screen.login_successful:  
+        home_screen.show()  # Show the HomeScreen if login is successful
+        app.exec()
+    else:
+        print("Login failed. Application exiting.")
+
+    # window.show()
+    # sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
